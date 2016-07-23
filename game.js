538d70bb8fdcde5271d1a7d42cb34e8ebc4c663b
vivek sengar <vivek@webcontentor.com>
2016-07-23 19:03:49 +0530

    changes done

--- game.js
+++ game.js
@@ -4,6 +4,46 @@ $(document).ready(function() {
     var isBoatRight = true;
     var whoIsInBoatFirst;
     var isRight = true;
+    var isPoliceRight = true;
+    var isPoliceInBoat = false;
+
+    function makePersonInBoatStateChange(whoIsInBoatFirst, isRight) {
+        var res = whoIsInBoatFirst.replace("#", "");
+        console.log("res", res);
+        var thiefstring = "isThiefRight";
+        var res1 = thiefstring.toLowerCase();
+
+        if (res1.indexOf(res) != -1) {
+            isThiefRight = isRight;
+            console.log('makePersonInBoatStateChange :: ', isThiefRight);
+        }
+    };
+    var doOnClickPersonRightNotInBoat = function(personId,location) {
+        $(personId).animate({marginLeft: location}, 500);
+        isThiefInBoat = true;
+        whoIsInBoatFirst = "#thief";
+        isThiefRight = true;
+        isPoliceRight = true;
+        isPoliceInBoat = true;
+    };
+    var doOnClickPersonRightInBoat = function(personId,location) {
+        $(personId).animate({marginLeft: location}, 500);
+        isThiefInBoat = false;
+        whoIsInBoatFirst = "";
+        isThiefRight = true;
+    };
+    var doOnClickPersonLeftInBoat = function(personId, location) {
+        $(personId).animate({marginLeft: location}, 500);
+        isThiefInBoat = false;
+        whoIsInBoatFirst = "";
+        isThiefRight = false;
+    };
+    var doOnClickPersonLeftNotInBoat = function(personId,location) {
+        $(personId).animate({marginLeft: location}, 500);
+        isThiefInBoat = true;
+        whoIsInBoatFirst = "#thief";
+        isThiefRight = false;
+    };
 
     $("#thief").click(function() {
         var thiefRightLocation = "0px";
@@ -27,6 +67,29 @@ $(document).ready(function() {
             }
         }
     });
+
+    $("#police").click(function() {
+        var policeRightLocation = "0px";
+        var policeInBoatRightLocation = "-250px";
+        var policeInBoatLeftLocation = "-650px";
+        var policeLeftLocation = "-950px";
+
+        if (isPoliceRight && isBoatRight) {
+            console.log(isPoliceRight && isBoatRight);
+            if (!isPoliceInBoat) {
+                doOnClickPersonRightNotInBoat("#police",policeInBoatRightLocation)
+            } else {
+                doOnClickPersonRightInBoat("#police", policeRightLocation)
+            }
+        }
+        if (!isPoliceRight && !isBoatRight) {
+            if (isPoliceInBoat) {
+                doOnClickPersonLeftInBoat("#police", policeLeftLocation)
+            } else {
+                doOnClickPersonLeftNotInBoat("#police", policeInBoatLeftLocation)
+            }
+        }    
+    });
     $("#let").click(function() {
         if (isBoatRight) {
             console.log("isBoatRight", isBoatRight);
@@ -47,39 +110,5 @@ $(document).ready(function() {
         }
     });
 
-    function makePersonInBoatStateChange(whoIsInBoatFirst, isRight) {
-        var res = whoIsInBoatFirst.replace("#", "");
-        console.log("res", res);
-        var thiefstring = "isThiefRight";
-        var res1 = thiefstring.toLowerCase();
-
-        if (res1.indexOf(res) != -1) {
-            isThiefRight = isRight;
-            console.log('makePersonInBoatStateChange :: ', isThiefRight);
-        }
-    };
-    var doOnClickPersonRightNotInBoat = function(personId,location) {
-        $(personId).animate({marginLeft: location}, 500);
-        isThiefInBoat = true;
-        whoIsInBoatFirst = "#thief";
-        isThiefRight = true;
-    };
-    var doOnClickPersonRightInBoat = function(personId,location) {
-        $(personId).animate({marginLeft: location}, 500);
-        isThiefInBoat = false;
-        whoIsInBoatFirst = "";
-        isThiefRight = true;
-    };
-    var doOnClickPersonLeftInBoat = function(personId, location) {
-        $(personId).animate({marginLeft: location}, 500);
-        isThiefInBoat = false;
-        whoIsInBoatFirst = "";
-        isThiefRight = false;
-    };
-    var doOnClickPersonLeftNotInBoat = function(personId,location) {
-        $(personId).animate({marginLeft: location}, 500);
-        isThiefInBoat = true;
-        whoIsInBoatFirst = "#thief";
-        isThiefRight = false;
-    };
+    
 });
